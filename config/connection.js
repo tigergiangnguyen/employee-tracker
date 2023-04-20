fd393600b7d729827ca9eff9c462759dd12dd87b
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-04-20 16:34:22 -0400

    Corrected db source

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
-const Sequelize = require('sequelize');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: process.env.DB_USER,
+        // MySQL password
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employee_db database.`)
 );
 
-module.exports = sequelize;
+module.exports = db;
